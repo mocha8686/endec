02070e155fe1f770e7d07ac1205f4d8a954ea5b6
mocha8686 <jericho.paulino@yahoo.com.ph>
2023-11-07 12:10:35 -0800

    caesar decode

--- src/lib/components/ciphers/Caesar.svelte
+++ src/lib/components/ciphers/Caesar.svelte
@@ -1,7 +1,11 @@
 <script lang="ts" context="module">
 	import { mod } from '$lib/mod';
 
-	export function caesar(input: string, shift: number): string {
+	export function caesar(
+		input: string,
+		shift: number,
+		mode: 'encode' | 'decode'
+	): string {
 		const ALPHABETS = [
 			'abcdefghijklmnopqrstuvwxyz',
 			'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
@@ -10,7 +14,9 @@
 		const shiftedAlphabets = ALPHABETS.reduce(
 			(acc: { [key: string]: string }, alphabet) => {
 				alphabet.split('').forEach((c, i) => {
-					acc[c] = alphabet.charAt(mod(i + shift, alphabet.length));
+					acc[c] = alphabet.charAt(
+						mod(mode === 'encode' ? i + shift : i - shift, alphabet.length)
+					);
 				});
 				return acc;
 			},
@@ -27,14 +33,39 @@
 <script lang="ts">
 	import Cipher from '$lib/components/Cipher.svelte';
 
-	import { input } from '$lib/input'
-	let shift = 3;
-	$: encoded = caesar($input, shift);
+	import { input } from '$lib/input';
+
+	let encodeShift = 3;
+	let decodeShift = 3;
+	let sync = true;
+
+	$: encoded = caesar($input, encodeShift, 'encode');
+	$: decoded = caesar($input, sync ? encodeShift : decodeShift, 'decode');
 </script>
 
-<Cipher name="Caesar" {encoded}>
-	<label slot="encoder-options">
-		Shift
-		<input type="number" min="0" max="26" bind:value={shift} />
-	</label>
+<Cipher name="Caesar" {encoded} {decoded}>
+	<svelte:fragment slot="encoder-options">
+		<label>
+			Shift
+			<input type="number" min="0" max="26" bind:value={encodeShift} />
+		</label>
+		<label>
+			Sync
+			<input type="checkbox" bind:checked={sync} />
+		</label>
+	</svelte:fragment>
+	<svelte:fragment slot="decoder-options">
+		<label>
+			Shift
+			{#if sync}
+				<input type="number" min="-26" max="26" bind:value={encodeShift} />
+			{:else}
+				<input type="number" min="-26" max="26" bind:value={decodeShift} />
+			{/if}
+		</label>
+		<label>
+			Sync
+			<input type="checkbox" bind:checked={sync} />
+		</label>
+	</svelte:fragment>
 </Cipher>
