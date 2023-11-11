34d6d71e07cc6f9d0a8f5e44596c803f65531dcb
mocha8686 <jericho.paulino@yahoo.com.ph>
2023-11-10 20:33:19 -0800

    text file upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,29 @@
 <script lang="ts">
 	import cipherGroups from '$lib/ciphers';
 	import { input } from '$lib/input';
+
+	let fileNode: HTMLInputElement;
+
+	async function useFile(_e: Event) {
+		const file = fileNode?.files?.[0];
+		if (!file) return;
+
+		if (file.type.startsWith('text/')) {
+			$input = (await file.text()).trim();
+		}
+	}
 </script>
 
 <form class="input-container">
 	<textarea class="textbox" bind:value={$input} />
+	<button
+		type="button"
+		class="textbox-button"
+		on:click={() => fileNode.click()}
+	>
+		Upload
+	</button>
+	<input type="file" accept="text/*" bind:this={fileNode} on:change={useFile} />
 </form>
 
 <ul class="ciphers">
@@ -23,9 +42,15 @@
 <style lang="scss">
 	@use '$lib/sass/abstracts/mixins' as *;
 
-	.textbox {
-		@include textbox(false);
+	.input-container {
+		@include textbox-grid(5.5em, 1);
+	}
 
+	.textbox {
 		width: 100%;
+	}
+
+	input[type='file'] {
+		display: none;
 	}
 </style>
